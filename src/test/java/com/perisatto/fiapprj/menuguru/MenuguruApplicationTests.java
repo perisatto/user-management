895fe7e60fa7133a9acdb4f806054b16baa3a2f4
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-04-28 22:48:19 -0300

    Initial commit

---
+++ src/test/java/com/perisatto/fiapprj/menuguru/MenuguruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perisatto.fiapprj.menuguru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuguruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
