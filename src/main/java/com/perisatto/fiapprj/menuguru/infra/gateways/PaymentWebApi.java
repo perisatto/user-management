b6d534b87117f1cc83d31e0ef2308ce13e020411
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-09-22 16:19:22 -0300

    Sonar issue

--- src/main/java/com/perisatto/fiapprj/menuguru/infra/gateways/PaymentWebApi.java
+++ src/main/java/com/perisatto/fiapprj/menuguru/infra/gateways/PaymentWebApi.java
@@ -79,13 +79,14 @@ public class PaymentWebApi implements PaymentProcessor {
 			throw new ValidationException("pymt-1000", "Error during payment processes. Please refer to log application for details.");
 		}
 		
-		if(response.getBody().getInStoreOrderId() != null) {
-			payment.setId(response.getBody().getInStoreOrderId());
+		if(response.getBody() != null) {
+			if(response.getBody().getInStoreOrderId() != null ) {
+				payment.setId(response.getBody().getInStoreOrderId());
+			}
 			
-		}
-		
-		if(response.getBody().getQrData() != null) {
-			payment.setPaymentLocation(response.getBody().getQrData());
+			if(response.getBody().getQrData() != null) {
+				payment.setPaymentLocation(response.getBody().getQrData());
+			}
 		}
 		
 		return payment;
