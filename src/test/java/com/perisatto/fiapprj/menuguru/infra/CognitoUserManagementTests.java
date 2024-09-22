5b9e1b0cc95d8a716dbdaafa0df7846b21d41ba8
Rodrigo Perisatto <rodrigo@perisatto.com>
2024-09-22 15:56:11 -0300

    100%

--- src/test/java/com/perisatto/fiapprj/menuguru/infra/CognitoUserManagementTests.java
+++ src/test/java/com/perisatto/fiapprj/menuguru/infra/CognitoUserManagementTests.java
@@ -1,5 +1,9 @@
 package com.perisatto.fiapprj.menuguru.infra;
 
+import static org.assertj.core.api.Assertions.assertThatException;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.catchException;
+
 import java.util.Random;
 
 import org.junit.jupiter.api.Test;
@@ -9,6 +13,7 @@ import org.springframework.core.env.Environment;
 import org.springframework.test.context.ActiveProfiles;
 
 import com.perisatto.fiapprj.menuguru.domain.entities.user.User;
+import com.perisatto.fiapprj.menuguru.handler.exceptions.ValidationException;
 import com.perisatto.fiapprj.menuguru.infra.gateways.CognitoUserManagement;
 
 @SpringBootTest
@@ -31,5 +36,46 @@ public class CognitoUserManagementTests {
 		user.setPassword("00000000000");
 		
 		cognitoUserManagement.createUser(user);
+	}
+	
+	@Test
+	void givenDuplicateData_thenRefusesCreateUser() throws Exception {	
+		CognitoUserManagement cognitoUserManagement = new CognitoUserManagement(env);
+		
+		Random random = new Random();
+		
+		User user = new User();
+		user.setEmail("teste_" + random.nextInt() + "@testesunitarios.com.br");
+		user.setName("Nome Sobrenome");
+		user.setId(999999L);
+		user.setPassword("00000000000");
+		
+		cognitoUserManagement.createUser(user);
+		
+		//tries to recreate the existent user
+		try {
+			cognitoUserManagement.createUser(user);
+		} catch (Exception e) {
+			assertThatExceptionOfType(ValidationException.class);
+		}		
+	}
+	
+	@Test
+	void givenInvalidData_thenRefusesCreateUser() throws Exception {	
+		CognitoUserManagement cognitoUserManagement = new CognitoUserManagement(env);
+		
+		Random random = new Random();
+		
+		User user = new User();
+		user.setEmail("teste_" + random.nextInt() + "@testesunitarios.com.br");
+		user.setName("Nome Sobrenome");
+		user.setId(999999L);
+		user.setPassword("0000000000");
+		
+		try {
+			cognitoUserManagement.createUser(user);
+		} catch (Exception e) {
+			assertThatExceptionOfType(ValidationException.class);
+		}
 	}
 }
